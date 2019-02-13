3324ed47d81d8a173de119f7dd1707fada71917b
artcool <710760186@qq.com>
2019-02-13 18:07:57 +0800

    联系我们页面，缺百度地图跟侧边导航栏

--- src/pages/callus/index.jsx
+++ src/pages/callus/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import CSSModules from 'react-css-modules';
+import CU from './callus.styl';
 class CallUs extends Component {
   constructor(props) {
     super(props);
@@ -13,11 +14,21 @@ class CallUs extends Component {
   render() {
     return (
       <div className="index-page">
-        <Button variant="contained" color="primary">
-          联系我们
-        </Button>
+        <div styleName="phone-number">
+          <img src={require("../../assets/images/201703031532113211.jpg")} alt="phone"/>
+          <p>电话 : 0755-82403817</p>
+        </div>
+        <div styleName="fax">
+          <img src={require("../../assets/images/201703031532203220.jpg")} alt="phone"/>
+          <p>传真 : 0755-82403817</p>
+        </div>
+        <div styleName="address">
+          <img src={require("../../assets/images/201703031532293229.jpg")} alt="phone"/>
+          <p>地址 : 深圳市罗湖区上步北路2006号</p>
+        </div>
+        
       </div>
     );
   }
 }
-export default CallUs;
+export default CSSModules(CallUs, CU, { "allowMultiple": true });
