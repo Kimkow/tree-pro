ddf21d9e7dce1eef4fd995c8afb040953eee036c
程皓 <hao.3248@163.com>
2019-06-12 14:29:57 +0800

    修改title

--- src/pages/miaomu/index.jsx
+++ src/pages/miaomu/index.jsx
@@ -1,22 +1,22 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CSSModules from 'react-css-modules';
 import MS from './miaomu.styl';
 import MinMenu from '../../components/minMenu';
 import Hideen from '@material-ui/core/Hidden';
-import {getMenu, getList, getInfo, getAllList} from '../../api/miaomu';
+import { getMenu, getList, getInfo, getAllList } from '../../api/miaomu';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Detail from '../../components/detail';
 
 const Content = (props) => {
-  const {listData, loading} = props;
+  const { listData, loading } = props;
   const [isLoading, setIsLoading] = useState(loading);
   const [showInfo, setShowInfo] = useState(false);
   const [detail, setDetail] = useState({});
   const [contentList, setContentList] = useState(listData);
   const handlerClick = (id) => {
     setIsLoading(true)
-    getInfo({id}).then(req => {
+    getInfo({ id }).then(req => {
       setIsLoading(false);
       setDetail(req);
       setShowInfo(true);
@@ -27,7 +27,7 @@ const Content = (props) => {
   };
   const checkDataLength = (list) => {
     let status;
-    if (list.data && list.data.length > 3) {
+    if (list.data && list.data.length > 4) {
       if (list.show) {
         status = false;
       } else {
@@ -54,55 +54,62 @@ const Content = (props) => {
             left: '50%',
             transform: 'translateX(-50%)',
             zIndex: 99999
-          }}/>
+          }} />
         </div>
         :
         showInfo ?
-          <Detail isMaiomu detailObj={{images: [], head: detail.title, text: detail.text}}
-                  video={detail.video && detail.video.path}/>
+          <Detail isMaiomu detailObj={{ images: [], head: detail.title, text: detail.text }}
+            video={detail.video && detail.video.path} />
           :
           <div styleName="list-content">
             {
               contentList.map((list, index) => {
                 return (
-                  <div key={index} style={{position: 'relative'}}>
-                    <h1 styleName="list-title">{list.title}</h1>
-                    {checkDataLength(list) ? <Button styleName="more-btn body-button" onClick={() => {
-                      handleShowAll(index)
-                    }}>查看更多</Button> : ''}
-                    <div styleName="list">
-                      {
-                        list.data && list.data.length > 0
-                          ?
-                          list.data.map((o, i) => {
-                            if (list.show) {
-                              return (
-                                <div key={i} onClick={() => {
-                                  handlerClick(o.id)
-                                }}>
-                                  <p>{o.title}</p>
-                                  <img src={o.titlePicturePath} alt=""/>
-                                </div>
-                              )
-                            } else {
-                              if (i < 4) {
-                                return (
-                                  <div key={i} onClick={() => {
-                                    handlerClick(o.id)
-                                  }}>
-                                    <p>{o.title}</p>
-                                    <img src={o.titlePicturePath} alt=""/>
-                                  </div>
-                                )
-                              } else {
-                                return ''
-                              }
+                  <div key={index} style={{ position: 'relative' }}>
+                    <h1 styleName="list-head">{list.parent}</h1>
+                    {list.listData.map((child, childIndex) => {
+                      return (
+                        <div key={childIndex}>
+                          <h3 styleName="list-title">{child.title}</h3>
+                          {checkDataLength(child) ? <Button styleName="more-btn body-button" onClick={() => {
+                            handleShowAll(child, childIndex)
+                          }}>查看更多</Button> : ''}
+                          <div styleName="list">
+                            {
+                              child.data && child.data.length > 0
+                                ?
+                                child.data.map((o, i) => {
+                                  if (child.show) {
+                                    return (
+                                      <div key={i} onClick={() => {
+                                        handlerClick(o.id)
+                                      }}>
+                                        <p>{o.title}</p>
+                                        <img src={o.titlePicturePath} alt="" />
+                                      </div>
+                                    )
+                                  } else {
+                                    if (i < 4) {
+                                      return (
+                                        <div key={i} onClick={() => {
+                                          handlerClick(o.id)
+                                        }}>
+                                          <p>{o.title}</p>
+                                          <img src={o.titlePicturePath} alt="" />
+                                        </div>
+                                      )
+                                    } else {
+                                      return ''
+                                    }
+                                  }
+                                })
+                                :
+                                '无数据'
                             }
-                          })
-                          :
-                          '无数据'
-                      }
-                    </div>
+                          </div>
+                        </div>
+                      )
+                    })}
                   </div>
                 )
               })
@@ -132,44 +139,59 @@ class MiaoMu extends Component {
   }
 
   getMenuList() {
-    this.setState({menuLoading: true});
+    this.setState({ menuLoading: true });
     getMenu().then(req => {
-      this.setState({menuLoading: false});
-      this.setState({minMenuData: req || []});
+      this.setState({ menuLoading: false });
+      this.setState({ minMenuData: req || [] });
       this.getAllContentList();
     }).catch(_ => {
       console.log(_);
-      this.setState({menuLoading: false});
+      this.setState({ menuLoading: false });
     })
   }
 
   getAllContentList() {
-    this.setState({listLoading: true});
+    this.setState({ listLoading: true });
     getAllList().then(req => {
-      this.setState({listLoading: false});
+      this.setState({ listLoading: false });
       let listData = [];
-      req.forEach((o, i) => {
+      req.forEach((item, index) => {
+        let data = {
+          parent: item.parent,
+          listData: []
+        };
+        item.children.forEach((o, i) => {
+          data.listData.push({
+            title: o.menuName,
+            id: o.menuId,
+            data: o.articleListVOList
+          })
+        });
+        listData.push(data);
+      });
+      console.log(listData);
+      /* req.forEach((o, i) => {
         listData.push({
           title: o.menuName,
           id: o.menuId,
           data: o.articleListVOList
         })
-      });
-      this.setState({listData})
+      }); */
+      this.setState({ listData })
     }).catch(_ => {
       console.log(_);
-      this.setState({listLoading: false});
+      this.setState({ listLoading: false });
     })
   }
 
   getContentList(id, title) {
-    this.setState({listLoading: true});
-    getList({id}).then(req => {
-      this.setState({listLoading: false});
-      this.setState({listData: [{data: req || [], title, show: true}]})
+    this.setState({ listLoading: true });
+    getList({ id }).then(req => {
+      this.setState({ listLoading: false });
+      this.setState({ listData: [{ parent: '', listData: [{ data: req || [], title, show: true }] }] })
     }).catch(_ => {
       console.log(_);
-      this.setState({listLoading: false});
+      this.setState({ listLoading: false });
     })
   }
 
@@ -178,7 +200,7 @@ class MiaoMu extends Component {
   }
 
   handleChildData(data) {
-    let {value, name} = data;
+    let { value, name } = data;
     this.getContentList(value, name);
   }
 
@@ -209,7 +231,7 @@ class MiaoMu extends Component {
   render() {
     let path = this.props.match.path.split(':')[0];
     let activeIndex = this.props.match.params.id; // 当前子菜单ID
-    const ContentMS = CSSModules(Content, MS, {"allowMultiple": true});
+    const ContentMS = CSSModules(Content, MS, { "allowMultiple": true });
     return (
       <div styleName="container">
         {this.state.menuLoading && <CircularProgress size={30} thickness={5} style={{
@@ -219,29 +241,29 @@ class MiaoMu extends Component {
           left: '50%',
           transform: 'translateX(-50%)',
           zIndex: 99999
-        }}/>}
-        {this.state.menuLoading && <div className="mask"/>}
+        }} />}
+        {this.state.menuLoading && <div className="mask" />}
         <Hideen smDown>
           <div styleName="menu">
             <div styleName="img-group">
-              <img src={require('../../assets/images/miaomu/miaomu_title.png')} alt=""/>
-              <img style={{cursor: 'pointer'}} src={require('../../assets/images/icont_tip_bg2.png')} alt=""
-                   onClick={() => {
-                     this.getAllContentList()
-                   }}/>
+              <img src={require('../../assets/images/miaomu/miaomu_title.png')} alt="" />
+              <img style={{ cursor: 'pointer' }} src={require('../../assets/images/icont_tip_bg2.png')} alt=""
+                onClick={() => {
+                  this.getAllContentList()
+                }} />
             </div>
             <MinMenu isMaiomu listData={this.state.minMenuData} menuPath={path} activeIndex={activeIndex}
-                     handlerData={this.handleChildData}/>
+              handlerData={this.handleChildData} />
           </div>
         </Hideen>
         <Hideen mdUp>
           <MinMenu listData={this.state.minMenuData} menuPath={path} activeIndex={activeIndex}
-                   handlerData={this.handleChildData}/>
+            handlerData={this.handleChildData} />
         </Hideen>
-        <ContentMS loading={this.state.listLoading} listData={this.state.listData}/>
+        <ContentMS loading={this.state.listLoading} listData={this.state.listData} />
       </div>
     );
   }
 }
 
-export default CSSModules(MiaoMu, MS, {"allowMultiple": true});
+export default CSSModules(MiaoMu, MS, { "allowMultiple": true });
